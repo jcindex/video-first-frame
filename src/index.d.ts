5fed05af233681e519aa2499149f22d750ac0b88
chenjincan <chenjincan@51songguo.com>
2022-04-25 14:44:51 +0800

    优化模块导出; 添加质量控制功能;

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,11 +1,12 @@
 
-export class VideoFirstFrame {
+export const VideoFirstFrame = {
 
     /**
      * 获取视频首帧图片
      * @param urlOrEl 文件或Video DOM对象
-     * @param returnBlob 是否返回Blob对象
+     * @param returnBlob 是否返回Blob对象，默认为true
+     * @param {number} quality 输出图片质量，0-1，默认为1
      */
-    static get(urlOrEl: HTMLVideoElement | File, returnBlob: boolean): Promise<Blob | string>
+    get(urlOrEl: HTMLVideoElement | File, returnBlob: boolean, quality: number): Promise<Blob | string>
 
 }
--- src/index.js
+++ src/index.js
@@ -1,20 +1,44 @@
+const defer = function () {
+  const obj = {};
+  obj.promise = new Promise(function (resolve, reject) {
+    obj.resolve = resolve;
+    obj.reject = reject;
+  });
+  return obj;
+};
 
-const defer = () => {
-    const obj = {}
-    obj.promise = new Promise((resolve, reject) => {
-        obj.resolve = resolve
-        obj.reject = reject
-    })
-    return obj
+function video2canvas(video, returnBlob, quality, done) {
+  const canvas = document.createElement("canvas")
+  canvas.width = video.videoWidth
+  canvas.height = video.videoHeight
+  const ctx = canvas.getContext("2d")
+  ctx.drawImage(video, 0, 0)
+  if (!returnBlob) {
+    done(canvas.toDataURL("image/png", 1))
+    return
+  }
+  canvas.toBlob(
+    function (blob) {
+      const rname = Math.random().toString(16).slice(2)
+      const file = new window.File([blob], `first-frame-${rname}.png`, {
+        type: "image/png"
+      })
+      done(file)
+      URL.revokeObjectURL(blob) // 释放Blob
+    },
+    "image/png",
+    quality
+  )
 }
 
-exports.VideoFirstFrame = class VideoFirstFrame {
+export const VideoFirstFrame = {
   /**
    * 获取视频首帧
    * @param {HTMLVideoElement} urlOrEl
    * @param {boolean} returnBlob
+   * @param {number} quality 输出图片质量,0-1
    */
-  static get(urlOrEl, returnBlob = true) {
+  get(urlOrEl, returnBlob = true, quality = 1) {
     if (!(urlOrEl instanceof HTMLVideoElement)) {
       let url = urlOrEl
       if (url instanceof File) {
@@ -28,44 +52,16 @@ exports.VideoFirstFrame = class VideoFirstFrame {
       urlOrEl.play()
     }
     const defered = defer()
-    urlOrEl.oncanplay = (e) => {
-      VideoFirstFrame.#video2canvas(e.target, returnBlob, defered.resolve)
+    urlOrEl.oncanplay = function (e) {
+      video2canvas(e.target, returnBlob, quality, defered.resolve)
     }
     urlOrEl.onerror = function (e) {
       defered.reject(e)
     }
-    return defered.promise.then((res) => {
+    return defered.promise.then(function (res) {
       // 移除元素
       document.body.removeChild(urlOrEl)
       return res
     })
-  }
-
-  /**
-   *
-   * @param {HTMLVideoElement} video
-   */
-  static #video2canvas(video, returnBlob, done) {
-    const canvas = document.createElement("canvas")
-    canvas.width = video.videoWidth
-    canvas.height = video.videoHeight
-    const ctx = canvas.getContext("2d")
-    ctx.drawImage(video, 0, 0)
-    if(!returnBlob) {
-        done(canvas.toDataURL("image/png", 1))
-        return
-    }
-    canvas.toBlob(
-      (blob) => {
-        const rname = Math.random().toString(16).slice(2)
-        const file = new window.File([blob], `first-frame-${rname}.png`, {
-          type: "image/png"
-        })
-        done(file)
-        URL.revokeObjectURL(blob) // 释放Blob
-      },
-      "image/png",
-      1
-    )
   }
 }
